fc748fd37b514971444283b591d68e73b6775b98
Nikita Tkachenko <mercartar@gmail.com>
2023-07-26 21:45:58 +0300

    drunken speech

--- Multithreading playground/Multithreading playground.cpp
+++ Multithreading playground/Multithreading playground.cpp
@@ -4,66 +4,53 @@
 #include <random>
 #include <string>
 #include <shared_mutex>
+#include <future>
 using namespace std;
 random_device randint;
 mt19937 engine(randint());
-uniform_int_distribution<int> distr(0, 1);
-uniform_int_distribution<int> ind(0, 4);
+uniform_int_distribution<int> binary_distr(0, 1);
 
-shared_mutex task_mutex;
 
-void add_binary(vector<int>& binary) {
-    lock_guard<shared_mutex> lock(task_mutex);
-    int value = distr(engine);
-    binary.push_back(value);
-    cout << "Wrote number " <<  value << " to the end" << endl;
-    this_thread::sleep_for(800ms);
-}
-void read_binary(const vector<int>& binary) {
-    shared_lock<shared_mutex> lock(task_mutex);
-    if (binary.empty()) {
-        cout << "Empty" << endl;
-        return;
-    }
-    int octet = 0;
-    for (auto i : binary) {
-        cout << i;
-        octet++;
-        if (octet == 4) {
-            cout << ' ';
-            octet = 0;
+void produce(const string& message, promise<string>& produce_string) {
+    string produced;
+    int size = message.size();
+    for (int i = 0; i < size; i++) {
+        uniform_int_distribution<int> distr(0, size-1);
+        int index = distr(engine);
+        char c;
+        if (message[index] == ' ') {
+            c = ' ';
         }
+        else {
+            int choise = binary_distr(engine);
+            c = (choise == 0) ? toupper(message[index]) : tolower(message[index]);
+        }
+        produced.push_back(c);
+        cout << "/";
+        this_thread::sleep_for(120ms);
     }
     cout << endl;
-
+    cout << "Finished mutilating string!" << endl;
+    produce_string.set_value(produced);
 }
-
+void consume(future<string>& consume_string) {
+    string output = consume_string.get();
+    for (auto c : output) {
+            this_thread::sleep_for(120ms);
+            cout << c;
+    }
+}
 
 int main()
 {
-    cout << "Enter how many threads would you like to launch" << ' ';
-    int num;
-    cin >> num;
-    cin.ignore();
-    vector<int> data;
-    vector <thread> threads;
-    for (int i{}; i < num; i++) {
-        int choise = distr(engine);
-        switch (choise) {
-        case 0:
-            threads.push_back(thread(add_binary, ref(data)));
-            threads[i].join();
-            break;
-        case 1:
-            threads.push_back(thread(read_binary, ref(data)));
-            threads[i].join();
-            break;
-        default:
-            continue;
-            break;
-        }
-        
-    }
+    string input;
+    getline(cin, input);
+    promise<string> produce_string;
+    future<string> consume_string = produce_string.get_future();
+    thread produce_thread(produce, ref(input), ref(produce_string));
+    thread consume_thread(consume, ref(consume_string));
+    produce_thread.join();
+    consume_thread.join();
     return 0;
 }
 
